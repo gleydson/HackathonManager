07a10bf45ee4f9082baf344bcd060b0f91dc0324
gleydson <gleydsonsr@gmail.com>
2018-05-16 04:01:00 +0000

    first commit

---
+++ src/main/java/com/greenmile/challenger/HackathonManagerApplication.java
@@ -0,0 +1,12 @@
+package com.greenmile.challenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greenmile/challenger/HackathonManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenmile.challenger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HackathonManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
